3b68628c4b5d5614217915e05054911ba7f41474
Thon Ly <thonly@heartbank.ceo>
2021-11-29 10:41:39 -0800

    version 2

--- khmer.js
+++ khmer.js
@@ -2,3 +2,9 @@ function changeLanguage(event) {
     //console.log(event.target.value)
     document.location = event.target.value
 }
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TR7EB8HM4Z');
