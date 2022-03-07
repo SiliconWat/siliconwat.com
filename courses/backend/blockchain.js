10681010a31ab1e0608b368a8a9b93b7c6aa2972
Thon Ly <thonly@heartbank.ceo>
2022-03-07 07:45:20 -0800

    fixed mobile css

--- courses/backend/blockchain.js
+++ courses/backend/blockchain.js
@@ -1,3 +1,7 @@
+function connectWallet() {
+  alert("Commitment Deposit Smart Contract currently in development at HeartBank®");
+}
+
 window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
