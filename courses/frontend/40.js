4b611e24355df20d94e2f680be3e21c5c64962fe
Thon Ly <thonly@heartbank.ceo>
2022-05-24 16:04:12 -0700

    begin 40 series

---
+++ courses/frontend/40.js
@@ -0,0 +1,9 @@
+function connectWallet() {
+  alert("Commitment Deposit Smart Contract currently in development at HeartBank®");
+}
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TR7EB8HM4Z');
