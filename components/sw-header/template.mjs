0658287ff2becd5f3a5d943fa0230aac24465c93
Thon Ly <thonly@heartbank.ceo>
2022-10-14 14:34:34 -0700

    begin v2

---
+++ components/sw-header/template.mjs
@@ -0,0 +1,175 @@
+const template = document.createElement("template");
+
+template.innerHTML = `
+    <link rel="stylesheet" href="components/sw-header/shadow.css">
+    <header>
+        <section>
+            <a href="/"><img src="siliconwat.png"></a>
+            <a href="/"><h1>Silicon Wat University</h1></a>
+        </section>
+        <input id="menu-toggle" type="checkbox">
+        <label class='menu-button-container' for="menu-toggle">
+            <span class='menu-button'></span>
+        </label>
+        <nav>
+            <ul>
+                <li>
+                    <h3>Frontend Course</h3>
+                    <div>
+                        <a href="#frontend-cohort"><button>Join: Remote Cohort Frontend Program</button></a>
+                        <h4>Learn</h4>
+                        <menu>
+                            <li><a>Watch Videos on Udemy</a></li>
+                            <li><a>Read Textbook on Medium</a></li>
+                            <li><a href="#frontend-quiz">Take Quizzes to Check Understanding</a></li>
+                        </menu>
+                        <h4>Practice</h4>
+                        <menu>
+                            <li><a href="#frontend-podcast">Podcast: Motivation & Coding Music</a></li>
+                            <li><a href="#frontend-code">Interactive Coding Exercises</a></li>
+                            <li><a>Remote Pair Programming on Discord</a></li>
+                            <li><a>Students' Solo Projects on Codepen</a></li>
+                        </menu>
+                        <h4>Review</h4>
+                        <menu>
+                            <li><a>Live Demo: Music Apps</a></li>
+                            <li><a>Syntax Flashcards</a></li>
+                            <li><a>Group Chat & Discussion Boards</a></li>
+                            <li><a>Students' Writings on Medium</a></li>
+                            <li><a>Mock Interviews on YouTube</a></li>
+                        </menu>
+                        <h4>Bonus: Advanced Lessons</h4>
+                        <menu>
+                            <li><a>React and Redux</a></li>
+                            <li><a>Web Audio Production</a></li>
+                        </menu>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <h3>Backend Course</h3>
+                    <div>
+                        <a href="#backend-cohort"><button>Join: Remote Cohort Backend Program</button></a>
+                        <h4>Learn</h4>
+                        <menu>
+                            <li><a>Watch Videos on Udemy</a></li>
+                            <li><a>Read Textbook on Medium</a></li>
+                            <li><a href="#backend-quiz">Take Quizzes to Check Understanding</a></li>
+                        </menu>
+                        <h4>Practice</h4>
+                        <menu>
+                            <li><a href="#backend-podcast">Podcast: Motivation & Coding Music</a></li>
+                            <li><a href="#backend-code">Interactive Coding Exercises</a></li>
+                            <li><a>Remote Pair Programming on Discord</a></li>
+                            <li><a>Students' Solo Projects on Replit</a></li>
+                        </menu>
+                        <h4>Review</h4>
+                        <menu>
+                            <li><a>Live Demo: Music Apps</a></li>
+                            <li><a>Syntax Flashcards</a></li>
+                            <li><a>Group Chat & Discussion Boards</a></li>
+                            <li><a>Students' Writings on Medium</a></li>
+                            <li><a>Mock Interviews on YouTube</a></li>
+                        </menu>
+                        <h4>Bonus: Advanced Lessons</h4>
+                        <menu>
+                            <li><a>Quantum Physics & Computing</a></li>
+                            <li><a>Kiitos Programming Language</a></li>
+                        </menu>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <h3>iOS Course</h3>
+                    <div>
+                        <a href="#ios-cohort"><button>Join: Remote Cohort iOS Program</button></a>
+                        <h4>Learn</h4>
+                        <menu>
+                            <li><a>Watch Videos on Udemy</a></li>
+                            <li><a>Read Textbook on Medium</a></li>
+                            <li><a href="#ios-quiz">Take Quizzes to Check Understanding</a></li>
+                        </menu>
+                        <h4>Practice</h4>
+                        <menu>
+                            <li><a href="#ios-podcast">Podcast: Motivation & Coding Music</a></li>
+                            <li><a href="#ios-code">Interactive Coding Exercises</a></li>
+                            <li><a>Remote Pair Programming on Discord</a></li>
+                            <li><a>Students' Solo Projects on GitHub</a></li>
+                        </menu>
+                        <h4>Review</h4>
+                        <menu>
+                            <li><a>Live Demo: Music Apps</a></li>
+                            <li><a>Syntax Flashcards</a></li>
+                            <li><a>Group Chat & Discussion Boards</a></li>
+                            <li><a>Students' Writings on Medium</a></li>
+                            <li><a>Mock Interviews on YouTube</a></li>
+                        </menu>
+                        <h4>Bonus: Advanced Lessons</h4>
+                        <menu>
+                            <li><a>3D Animations</a></li>
+                            <li><a>Deep Learning</a></li>
+                        </menu>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <h3>Graduation</h3>
+                    <div>
+                        <button>Silicon Wat Blockchain</button>
+                        <h4>Awards</h4>
+                        <menu>
+                            <li><a>Course Completion Certification</a></li>
+                            <li><a>Graduation Diploma</a></li>
+                        </menu>
+                        <h4>Rewards</h4>
+                        <menu>
+                            <li><a>Committment Deposit Refund</a></li>
+                            <li><a>Course Completion Bonus</a></li>
+                            <li><a>Graduation Bonus</a></li>
+                        </menu>
+                        <h4>Benefits</h4>
+                        <menu>
+                            <li><a>Mock Technical Interviews</a></li>
+                            <li><a>Work Bounties at THonly™</a></li>
+                            <li><a>Startup Incubator Admission</a></li>
+                        </menu>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <h3>Coupons</h3>
+                    <div>
+                        <a href="#coupon"><button>Apply for Discounts</button></a>
+                        <h4>Inclusion & Diversity</h4>
+                        <menu>
+                            <li><a>Women & Minorities</a></li>
+                            <li><a>Ukrainian Refugees</a></li>
+                            <li><a>Cambodian Genocide</a></li>
+                        </menu>
+                        <h4>Campuses</h4>
+                        <menu>
+                            <li><a>Cambodia</a></li>
+                            <li><a>Ukraine</a></li>
+                            <li><a>United States</a></li>
+                        </menu>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <aside>
+            <select onchange="this.getRootNode().host.changeLanguage(event)">
+                <option value="en">English</option>
+                <option value="km" disabled>Khmer</option>
+                <option value="es" disabled>Spanish</option>
+                <option value="ru" disabled>Russian</option>
+                <option value="zh" disabled>Chinese</option>
+            </select>
+        </aside>
+    </header>
+`;
+
+export default template;
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function changeLanguage(event) {
-    //console.log(event.target.value)
-    document.location = event.target.value
-}
+import "./components/sw-header/element.mjs";
+
+import "./components/sw-main/sw-curriculum/element.mjs";
+import "./components/sw-main/sw-podcast/element.mjs";
+import "./components/sw-main/sw-quiz/element.mjs";
+import "./components/sw-main/sw-code/element.mjs";
+import "./components/sw-main/sw-cohort/element.mjs";
+import "./components/sw-main/sw-coupon/element.mjs";
+import "./components/sw-main/element.mjs";
+
+import "./components/sw-footer/element.mjs";
 
 window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-TR7EB8HM4Z');
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-TR7EB8HM4Z');
