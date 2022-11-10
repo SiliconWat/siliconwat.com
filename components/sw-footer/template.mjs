e51b57d693d29612878954744799dbf9a38a990d
Thon Ly <thonly@heartbank.ceo>
2022-11-10 08:13:31 -0800

    add svg background

--- components/sw-footer/template.mjs
+++ components/sw-footer/template.mjs
@@ -5,34 +5,48 @@ template.innerHTML = `
     <nav>
         <main>
             <section>
-                <h5>Sponsors</h5>
-                <ul class="donors">
-                    <li><a href="https://github.com/heartbank"><img src="https://github.com/heartbank.png?size=100"></a></li>
-                    <li><a href="https://github.com/siliconwat"><img src="https://github.com/siliconwat.png?size=100"></a></li>
-                    <li><a href="https://github.com/thonlymetaverse"><img src="https://github.com/thonlymetaverse.png?size=100"></a></li>
-                    <li><a href="https://github.com/nhiakou"><img src="https://github.com/nhiakou.png?size=100"></a></li>
-                </ul>
+                <div>
+                    <h5>Sponsors</h5>
+                    <ul class="donors">
+                        <li><a href="https://github.com/heartbank"><img src="https://github.com/heartbank.png?size=100"></a></li>
+                        <li><a href="https://github.com/siliconwat"><img src="https://github.com/siliconwat.png?size=100"></a></li>
+                        <li><a href="https://github.com/thonlymetaverse"><img src="https://github.com/thonlymetaverse.png?size=100"></a></li>
+                        <li><a href="https://github.com/nhiakou"><img src="https://github.com/nhiakou.png?size=100"></a></li>
+                    </ul>
+                </div>
             </section>
             <section>
-                <h5>Sponsor</h5>
-                <ul>
-                    <li><a href="https://heartbank.fund">HeartBank</a></li>
-                    <li><a href="https://github.com/sponsors/SiliconWat">GitHub</a></li>
-                </ul>
-                <h5>Contact</h5>
-                <ul>
-                    <li><a href="https://thonly.org">Lead Instructor: Thon Ly</a></li>
-                    <li><a href="mailto:university@siliconwat.com">university@siliconwat.com</a></li>
-                </ul>
+                <div>
+                    <h5>Thank</h5>
+                    <ul>
+                        <li><a href="https://kiitos.earth">Kiitos</a></li>
+                    </ul> 
+                </div>
+                <div>
+                    <h5>Sponsor</h5>
+                    <ul>
+                        <li><a href="https://heartbank.fund">HeartBank</a></li>
+                        <li><a href="https://github.com/sponsors/SiliconWat">GitHub</a></li>
+                    </ul>
+                </div>
             </section>
             <section>
-                <h5>Connect</h5>
-                <ul>
-                    <li><a href="https://talk.siliconwat.org">Silicon Wat</a></li>
-                    <li><a href="https://twitter.com/siliconwat">Twitter</a></li>
-                    <li><a href="https://youtube.com/@siliconwat">YouTube</a></li>
-                    <li><a href="https://medium.com/@thonly">Medium</a></li>
-                </ul>
+                <div>
+                    <h5>Connect</h5>
+                    <ul>
+                        <li><a href="https://talk.siliconwat.org">Silicon Wat</a></li>
+                        <li><a href="https://twitter.com/siliconwat">Twitter</a></li>
+                        <li><a href="https://youtube.com/@siliconwat">YouTube</a></li>
+                        <li><a href="https://medium.com/@thonly">Medium</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5>Contact</h5>
+                    <ul>
+                        <li><a href="https://thonly.org">Lead Instructor: Thon Ly</a></li>
+                        <li><a href="mailto:university@siliconwat.com">university@siliconwat.com</a></li>
+                    </ul>
+                </div>
             </section>
         </main>
         <footer>
--- components/sw-main/sw-stealth/template.mjs
+++ components/sw-main/sw-stealth/template.mjs
@@ -3,7 +3,7 @@ const template = document.createElement("template");
 template.innerHTML = `
     <link rel="stylesheet" href="components/sw-main/sw-stealth/shadow.css">
     <main>
-        <h1>Currently in stealth development...</h1>
+        <h1>Currently in development...</h1>
         <br>
         <a><button></button></a>
     </main>
