81d8df667bbef5944655193514255b8f06b88f44
Thon Ly <thonly@heartbank.ceo>
2022-12-26 16:50:49 -0800

    refactor

--- components/sw-main/sw-stealth/element.mjs
+++ components/sw-main/sw-stealth/element.mjs
@@ -12,7 +12,7 @@ class SwStealth extends HTMLElement {
         const a = this.shadowRoot.querySelector('a');
         const button = this.shadowRoot.querySelector('button');
         a.href = origin ? "https://" + origin : "https://medium.com/siliconwat/trilogy-curriculum-for-complete-beginners-aspiring-to-become-fullstack-engineers-28f3b03327d4?sk=b40d217c5ca765a93465bd2212d49025";
-        button.textContent = origin || "Trilogy Curriculum for Complete Beginners Aspiring to Become Fullstack Engineers";
+        button.textContent = origin || "Trilogy Curriculum for Complete Beginners";
     }
 }
 
