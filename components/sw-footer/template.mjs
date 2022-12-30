c25eb7b836bb98f737f99ddf73219e280a43114a
Thon Ly <thonly@heartbank.ceo>
2022-12-29 17:12:18 -0800

    refactor

--- components/sw-footer/template.mjs
+++ components/sw-footer/template.mjs
@@ -1,5 +1,5 @@
 import "./sw-sponsors/element.mjs";
-import { THONLY } from "/global.mjs";
+import { THONLY } from "https://thonly.org/global.mjs";
 const template = document.createElement("template");
 
 template.innerHTML = `
