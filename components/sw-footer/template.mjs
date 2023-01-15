afcc6d5de49521db47c22ba785fae9e6676efebd
Thon Ly <thonly@heartbank.ceo>
2023-01-15 09:20:48 -0800

    refactor

--- components/sw-footer/template.mjs
+++ components/sw-footer/template.mjs
@@ -48,8 +48,8 @@ template.innerHTML = `
             <select onchange="this.getRootNode().host.changeLanguage(event)">
                 <option value="en">English</option>
                 <option value="km" disabled>Khmer</option>
-                <option value="es" disabled>Spanish</option>
                 <option value="ru" disabled>Russian</option>
+                <option value="es" disabled>Spanish</option>
                 <option value="zh" disabled>Chinese</option>
             </select>
             <small><a href="https://siliconwat.org">Silicon Wat Campus</a> &copy; 2023</small>
